1b240b15ca396ba9efe06a1c9a73b5976c871cd7
Gabriel <gabriel.jimenez102@alu.ulpgc.es>
2016-05-02 15:41:15 +0200

    Horizontal hecho

--- heuristicas.py
+++ heuristicas.py
@@ -5,17 +5,49 @@ def h1(state):
     return random.randint(-100, 100)
 
 
-def derecha(state):
-    return 0
+def derecha(move, state):
+    result = 0
+    tx = move[0]
+    ty = move[1]
+    add = 100
+    for x in range(tx + 1, 7, 1):
+        if (x, ty) in state.board:
+            if state.board[(x, ty)] == state.to_move:
+                result += 2 * add
+            else:
+                return result
+        else:
+            result += add
+        add -= 10
+    return result
 
 
-def izquierda(state):
-    return 0
+def izquierda(move, state):
+    result = 0
+    tx = move[0]
+    ty = move[1]
+    add = 100
+    for x in range(tx - 1, 1, -1):
+        if (x, ty) in state.board:
+            if state.board[(x, ty)] == state.to_move:
+                result += 2 * add
+            else:
+                return result
+        else:
+            result += add
+        add -= 10
+    return result
 
 
 def horizontal(state):
-    return
+    result = 0
+    for move in state.moves:
+        result += derecha(move, state) + izquierda(move, state)
+    return result
 
 
 def h2(state):
-    return h1(state)
+    if state.to_move == 'X':
+        return horizontal(state)
+    else:
+        return -horizontal(state)
